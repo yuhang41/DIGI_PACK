9be0f647b5d35eb995cc8d1aed40cb3d6237fe0e
yuhang41 <etc70129@gmail.com>
2021-05-11 17:02:44 +0800

    05-11

--- js/step1.js
+++ js/step1.js
@@ -2,6 +2,7 @@
 // let commodity_Shoes = 0;
 // let commodity_Boots = 0;
 let sum = 0;
+let price_Content = [];
 let inputs = [...document.querySelectorAll("input")];
 let addbtns = [...document.querySelectorAll(".add")];
 let lessbtns = [...document.querySelectorAll(".less")];
@@ -10,8 +11,27 @@ let costs = [...document.querySelectorAll(".cost")];
 let prices = [...document.querySelectorAll(".price")];
 let count = inputs.length;
 
+let next_step = document.querySelector('.next_step');
+let product_name = [...document.querySelectorAll('.product_name')];
+let products = [];
+let contact = [];
+
+//讀取API資料
+axios.get("http://localhost:8888/contact")
+.then((res)=>{
+    contact = res.data;
+    console.log(contact)
+}).catch((err)=>{
+    console.log(err)
+})
+product_name.forEach((item)=>{
+    products.push(item);
+})
+
+// -------------計算input數量，費用
 prices.forEach((price)=>{
-    sum += price.textContent * 1
+    sum += price.textContent * 1;
+    price_Content.push(price.textContent * 1);
 })
 costsHandler();
 function costsHandler(){
@@ -25,7 +45,8 @@ addbtns.forEach((addbtn,index)=>{
         inputs[index].value ++;
         count ++;
         total.textContent = count;
-        sum += prices[index].textContent * 1;
+        prices[index].textContent = ((prices[index].textContent * 1) + price_Content[index]).toFixed(2);
+        sum += price_Content[index];
         costsHandler();
     })
 })
@@ -35,7 +56,24 @@ lessbtns.forEach((lessbtn,index)=>{
         inputs[index].value -= 1;
         count --;
         total.textContent = count;
-        sum -=prices[index].textContent * 1;
+        prices[index].textContent = ((prices[index].textContent * 1) - price_Content[index]).toFixed(2);
+        sum -= price_Content[index];
         costsHandler();
     })
 })
+
+function nextHandler(e){
+    e.preventDefault();
+    axios.put("http://localhost:8888/contact/2",{count,sum,price_Content})
+    .then((res)=>{
+        contact[1] = res.data;
+        nextPage();
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+function nextPage(){
+    window.location.href='http://127.0.0.1:5501/shopping_step2.html';
+}
+
+next_step.addEventListener('click',nextHandler)
